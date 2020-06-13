ca3ec3791740c1a09efb90c8c026468c4456b2d5
freddy9824 <f.castillo@correo.unimet.edu.ve>
2020-06-12 22:43:35 -0400

    Falto

--- src/proyectooperativos/Empleado.java
+++ src/proyectooperativos/Empleado.java
@@ -11,20 +11,42 @@ package proyectooperativos;
  */
 public class Empleado extends Thread{
     
-    private int cajas=3;
-    private int producto;
+    private int id;
+    private int productoEnCaja=3;
+    private int productoEnEstante;
+
+    public Empleado(int id) {
+        this.id = id;
+    }
+
+    public void setProductoEnEstante(int productoEnEstante) {
+        this.productoEnEstante = productoEnEstante;
+    }
     
+    
+    
+    @Override
     public void run() {
-        
+        traerCajas();
+        try {
+            sleep(5000); // 5minutos
+        }catch (InterruptedException e){
+            System.out.println("Error");
+        }
+        llenarEstante(productoEnEstante);
+        try {
+            sleep(1000); // 1 minuto
+        }catch (InterruptedException e){
+            System.out.println("Error");
+        }
     }
     
     private void traerCajas() {
         System.out.println("Ire al almacen");
     }
     
-    private int llenarEstante(int producto) {
-        return cajas+producto;
+    private int llenarEstante(int productoEnEstante) {
+        return productoEnCaja+productoEnEstante;
     }
     
-
 }
